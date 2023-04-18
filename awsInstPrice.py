d0637ffbd11714ccdf27b3bcfdda364b66bd634f
Pranav Prasanna <77316684+pranavp08@users.noreply.github.com>
2023-04-18 19:21:39 +0530

    Update awsInstPrice.py
    optimized getInstancePrice function, also querying for windows prices, edited insertIntoTable function to accept windows prices

--- awsInstPrice.py
+++ awsInstPrice.py
@@ -25,12 +25,13 @@ def ec2_instance_types(region_code):
             instanceName=i['InstanceType']
             instances[instanceName]=i
             try:
-                instanceFamily, price=getInstancePrice(instanceName,region_code)
+                instanceFamily, linuxPrice, windowsPrice=getInstancePrice(instanceName,region_code)
                 instances[instanceName]['InstanceFamily']=instanceFamily
-                instances[instanceName]['Price']=price
+                instances[instanceName]['LinuxPrice']=linuxPrice
+                instances[instanceName]['WindowsPrice']=windowsPrice
             except:
                 continue
-            
+
         if 'NextToken' not in describe_result:
             break
         describe_args['NextToken'] = describe_result['NextToken']
@@ -40,43 +41,36 @@ def ec2_instance_types(region_code):
 
 
 def getInstancePrice(instance_name, regionCode):
+    Filters = '[{{"Field": "tenancy", "Value": "shared", "Type": "TERM_MATCH"}},'\
+    '{{"Field": "operatingSystem", "Value": "{o}", "Type": "TERM_MATCH"}},'\
+    '{{"Field": "preInstalledSw", "Value": "NA", "Type": "TERM_MATCH"}},'\
+    '{{"Field": "instanceType", "Value": "{t}", "Type": "TERM_MATCH"}},'\
+    '{{"Field": "regionCode", "Value": "{r}", "Type": "TERM_MATCH"}},'\
+    '{{"Field": "capacitystatus", "Value": "Used", "Type": "TERM_MATCH"}},'\
+    '{{"Field": "licenseModel", "Value": "No License required", "Type": "TERM_MATCH"}}]'
 
-    if instance_name.startswith('p2'):
-        response = pricing.get_products(
-                ServiceCode='AmazonEC2',
-                Filters=[
-                    {'Type': 'TERM_MATCH', 'Field': 'instanceType', 'Value': instance_name},
-                    {'Type': 'TERM_MATCH', 'Field': 'operatingSystem', 'Value': 'Linux'},
-                    {'Type': 'TERM_MATCH', 'Field': 'tenancy', 'Value': 'Shared'},
-                    {'Type': 'TERM_MATCH', 'Field': 'regionCode', 'Value': regionCode},
-                    {'Type': 'TERM_MATCH', 'Field': 'preInstalledSw', 'Value': 'NA'},
-                ],
-                FormatVersion='aws_v1',
-            )
-    else:
-        response = pricing.get_products(
-                ServiceCode='AmazonEC2',
-                Filters=[
-                    {'Type': 'TERM_MATCH', 'Field': 'instanceType', 'Value': instance_name},
-                    {'Type': 'TERM_MATCH', 'Field': 'operatingSystem', 'Value': 'Linux'},
-                    {'Type': 'TERM_MATCH', 'Field': 'tenancy', 'Value': 'Shared'},
-                    {'Type': 'TERM_MATCH', 'Field': 'regionCode', 'Value': regionCode},
-                    {'Type': 'TERM_MATCH', 'Field': 'preInstalledSw', 'Value': 'NA'},
-                    {'Type': 'TERM_MATCH', 'Field': 'capacitystatus', 'Value': 'UnusedCapacityReservation'},
-                ],
-                FormatVersion='aws_v1',
-            )
+    f = Filters.format(r=regionCode, t=instance_name, o='Linux')
+    response = pricing.get_products(
+        ServiceCode='AmazonEC2', 
+        Filters=json.loads(f))
+    on_demand = json.loads(response['PriceList'][0])['terms']['OnDemand']
+    key1 = list(on_demand)[0]
+    key2 = list(on_demand[key1]['priceDimensions'])[0]
+    linuxPrice=on_demand[key1]['priceDimensions'][key2]['pricePerUnit']['USD']
+    linuxPrice=float(linuxPrice)
+    instanceFamily=json.loads(response['PriceList'][0])['product']['attributes']['instanceFamily']
 
-    for result in response['PriceList']:
-        json_result=json.loads(result)
-        for i in json_result['terms']['OnDemand'].values():
-            for j in i['priceDimensions'].values():
-                for k in j['pricePerUnit'].values():
-                    continue
-        price=float(k)
-        instanceFamily=json_result['product']['attributes']['instanceFamily']
-        
-        return instanceFamily, price
+    f = Filters.format(r=regionCode, t=instance_name, o='Windows')
+    response = pricing.get_products(
+        ServiceCode='AmazonEC2', 
+        Filters=json.loads(f))
+    on_demand = json.loads(response['PriceList'][0])['terms']['OnDemand']
+    key1 = list(on_demand)[0]
+    key2 = list(on_demand[key1]['priceDimensions'])[0]
+    windowsPrice=on_demand[key1]['priceDimensions'][key2]['pricePerUnit']['USD']
+    windowsPrice=float(windowsPrice)
+
+    return instanceFamily, linuxPrice, windowsPrice
 
 def insertIntoTable(instance_dict):
     query="delete from awsprice"
@@ -86,7 +80,8 @@ def insertIntoTable(instance_dict):
             instance=instance_dict[region][i]
             instance_name=i
             try:
-                price=instance['Price']
+                linuxPrice=instance['LinuxPrice']
+                windowsPrice=instance['WindowsPrice']
             except:
                 continue
             vcpu=instance['VCpuInfo']['DefaultVCpus']
@@ -98,8 +93,8 @@ def insertIntoTable(instance_dict):
             else:
                 storage='EBS only'
 
-            query="insert into awsprice values(%s, %s, %s, %s, %s, %s, %s)"
-            tup=(instance_name, price, vcpu, memory, storage, family, region)
+            query="insert into awsprice values(%s, %s, %s, %s, %s, %s, %s, %s)"
+            tup=(instance_name, linuxPrice, windowsPrice, vcpu, memory, storage, family, region)
             mycursor.execute(query,tup)
             mydb.commit()
 
@@ -110,26 +105,4 @@ instance_dict=dict()
 for region in region_list:
     instance_dict[region]=ec2_instance_types(region_code=region)
 
-
-#instance_dict['ap-south-2']=ec2_instance_types(region_code='ap-south-2')
 insertIntoTable(instance_dict)
-
-
-
-
-'''
-
-for i in instance_dict['ap-south-1']:
-    instance=instance_dict['ap-south-1'][i]
-    if 'InstanceStorageInfo' in instance.keys():
-        try:
-            print(i," $",instance['Price'], instance['VCpuInfo']['DefaultVCpus'], instance['MemoryInfo']['SizeInMiB']/1024,"GB", instance['InstanceStorageInfo']['TotalSizeInGB'],"GB SSD ", instance['InstanceFamily'])
-        except KeyError:
-            continue
-    else:
-        try:
-            print(i," $",instance['Price'], instance['VCpuInfo']['DefaultVCpus'], instance['MemoryInfo']['SizeInMiB']/1024,"GB", "EBS only", instance['InstanceFamily'])
-        except KeyError:
-            continue
-
-'''
