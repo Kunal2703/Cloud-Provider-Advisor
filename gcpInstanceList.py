54a96199c36407b034215273ded707c10377d7f8
Pranav Prasanna <77316684+pranavp08@users.noreply.github.com>
2023-04-19 21:59:39 +0530

    Delete gcpInstanceList.py

--- gcpInstanceList.py
+++
@@ -1,8 +0,0 @@
-from google.cloud import compute_v1, billing
-
-machine_client=compute_v1.MachineTypesClient()
-
-machine_list=machine_client.list(project='cloud-provider-advisor',zone='us-east4-c')
-for i in machine_list:
-    #print(i.name+" "+i.description)
-    print(i.name)
